793240f527300d38ade001ff2d6b61f217e45fa5
Otrebus <panzerschlacht@hotmail.com>
2014-06-11 15:54:48 +0200

    End of day commit. Untested (read non-working) code transmitter added.

--- Bytecode Uploader/src/se/neava/bytecodeuploader/BytecodeUploader.java
+++ Bytecode Uploader/src/se/neava/bytecodeuploader/BytecodeUploader.java
@@ -1,64 +1,199 @@
 package se.neava.bytecodeuploader;
 
-import java.io.PrintStream;
-import java.io.UnsupportedEncodingException;
-
 import jssc.SerialPort;
 import jssc.SerialPortEvent;
 import jssc.SerialPortException;
 import jssc.SerialPortEventListener;
+import jssc.SerialPortList;
 
-public class BytecodeUploader {
+public class BytecodeUploader 
+{
+	static final byte FRAME_DELIMITER = 0x7E;
+	static final byte ESCAPE_OCTET = 0x7D;
+	static final byte INITSEND_HEADER = 0x00;
+	static final byte MORESEND_HEADER = 0x01;
+	static final byte ACK_HEADER = 0x02;
+	static final byte ECHO_HEADER = 0x03;
+	
+	boolean escaping = false;
+	boolean sending = false;
+	enum ReceiveState { Idle, ExpectingHeader, ExpectingAckSeqLsb, ExpectingAckSeqMsb, ExpectingAckDelim, ExpectingData };
     
-    static SerialPort serialPort;
+	byte[] code;
+	ReceiveState receiveState;
+	int sendPtr = 0;
+	int ackSeq = 0;
+	int chunkSize = 0;
+	
+	SerialPort serialPort;
 
-    public static void main(String[] args) throws UnsupportedEncodingException, InterruptedException {
-
-        try {
-            serialPort = new SerialPort("COM4");
-
-            serialPort.openPort();//Open serial port
-            serialPort.setParams(SerialPort.BAUDRATE_9600, 
-                    SerialPort.DATABITS_8,
-                    SerialPort.STOPBITS_1,
-                    SerialPort.PARITY_NONE, false, false);//Set params. Also you can set params by this string: serialPort.setParams(9600, 8, 1, 0);
-            int mask = SerialPort.MASK_RXCHAR | SerialPort.MASK_CTS | SerialPort.MASK_DSR; //Prepare mask
-            serialPort.setEventsMask(mask); //Set mask
-            serialPort.addEventListener(new SerialPortReader());
-            System.out.println("Writing...");
-
-            //serialPort.writeBytes("~xtest~".getBytes("UTF-8"));//Write data to port
-            serialPort.writeBytes("YOYOYOYO~".getBytes("UTF-8"));
-            while(true);
-            //serialPort.closePort();//Close serial port
+    public BytecodeUploader(String portName, int baudRate) throws NoPortsFoundException, SerialPortException
+    {
+    	// If the port name was null, we're just going to take the first best port (convenience functionality)
+        if(portName == null)
+        {
+	    	String[] portNames = SerialPortList.getPortNames();
+	    	if(portNames.length == 0)
+	    		throw new NoPortsFoundException();
+	        portName = portNames[0];
         }
-        catch (SerialPortException ex) {
-            System.out.println(ex);
-        }
+        serialPort = new SerialPort(portName);
+        serialPort.openPort();
+        serialPort.setParams(baudRate, SerialPort.DATABITS_8, SerialPort.STOPBITS_1, 
+        		             SerialPort.PARITY_NONE, false, false);
+        serialPort.setEventsMask(SerialPort.MASK_RXCHAR);
+        serialPort.addEventListener(new SerialPortReader());
     }
     
-    static class SerialPortReader implements SerialPortEventListener {
-
-        public void serialEvent(SerialPortEvent event) {
-
-            System.out.println("got something!");
-            if(event.isRXCHAR())
-            {//If data is available
-                try {
-                    byte buffer[] = serialPort.readBytes(event.getEventValue());
-                    if(buffer.length > 0 && buffer[0] == '~')
-                    {
-                        serialPort.closePort();
-                        System.exit(0);
-                    }
-                    String blah = new String(buffer);
-                    System.out.print(blah);
+    public void finalize()
+    {
+    	try 
+    	{
+			serialPort.closePort();
+		} 
+    	catch (SerialPortException e) // Not much we can do
+		{
+    		e.printStackTrace();
+		}
+    }
+    
+    public void transmitCode(byte[] code, int chunkSize) throws SerialPortException, BusyException
+    {
+    	if(chunkSize > 255 || chunkSize < 1)
+    		throw new IllegalArgumentException("chunkSize must be in the interval [1, 255].");
+    	if(sending)
+    		throw new BusyException();
+    
+    	sending = true;
+    	this.code = code.clone();
+    	this.chunkSize = chunkSize;
+    	int dataLength = (byte) Math.min(chunkSize, code.length);
+    	byte bytes[] = { FRAME_DELIMITER, INITSEND_HEADER, (byte) dataLength};
+    	byte crc[] = { 0, 0, 0, 0 };
+    	
+    	serialPort.writeBytes(bytes);    	
+    	for(int i = 0; i < dataLength; i++)
+    		serialPort.writeByte(code[i]);
+    	sendPtr = dataLength;
+    	
+    	serialPort.writeBytes(crc);
+    	serialPort.writeByte(FRAME_DELIMITER);
+    }
+    
+    public void transmitMore() throws SerialPortException
+    {
+    	byte bytes[] = { FRAME_DELIMITER, MORESEND_HEADER };
+    	byte crc[] = { 0, 0, 0, 0 };
+    	 
+    	serialPort.writeBytes(bytes);
+    	for(int i = sendPtr; i < Math.min(sendPtr + chunkSize, code.length); i++)
+    		serialPort.writeByte(code[i]);
+    	sendPtr += chunkSize;
+    	
+    	serialPort.writeBytes(crc);
+    	serialPort.writeByte(FRAME_DELIMITER);
+    }
+    
+    private void handleAck() throws SerialPortException
+    {
+    	if(ackSeq >= code.length)
+    	{
+    		sending = false;
+    		sendPtr = 0;
+    		return;
+    	}
+    	if(ackSeq == sendPtr)
+    	{
+    		transmitMore();
+    	}
+    }
+    
+    private void handleReceivedByte(byte data) throws SerialPortException
+    {
+    	if(escaping)
+    	{
+    		data = (byte) (data ^ (1 << 5));
+    		escaping = false;
+    	}
+    	if(data == ESCAPE_OCTET)
+    	{
+    		escaping = true;
+    		return;
+    	}
+    	switch(receiveState)
+    	{
+    	case Idle:
+    		if(data == FRAME_DELIMITER)
+    			receiveState = ReceiveState.ExpectingHeader;
+    		else
+    			System.out.println(data);
+    		break;
+    	case ExpectingHeader:
+    		if(data == ACK_HEADER)
+    			receiveState = ReceiveState.ExpectingAckSeqLsb;
+    		else if(data == ECHO_HEADER)
+    			receiveState = ReceiveState.ExpectingData;
+    		break;
+    	case ExpectingAckSeqLsb:
+    		ackSeq = data;
+    		break;
+    	case ExpectingAckSeqMsb:
+    		ackSeq |= (int)data << 8;
+    		break;
+    	case ExpectingAckDelim:
+    		if(data == FRAME_DELIMITER)
+    		{
+    			receiveState = ReceiveState.Idle;
+    			handleAck();
+    		}
+    		else
+    			System.out.println("Expected end delimiter, got something else.");
+    		break;
+    	case ExpectingData:
+    		if(data == FRAME_DELIMITER)
+    		{
+    			System.out.println("");
+    			receiveState = ReceiveState.Idle;
+    			return;
+    		}
+    		else
+    			System.out.print(data);
+    		break;
+    	default:
+    		System.out.println("Bug!! Bad state!");
+    		break;
+    	}
+    }
+    
+    private class SerialPortReader implements SerialPortEventListener 
+    {    	
+        public void serialEvent(SerialPortEvent event) 
+        {
+            if(event.isRXCHAR()) // Received some bytes!
+            {
+            	if(sending)
+            		System.out.println("Received data while sending, device seems to be misbehaving.");
+                try 
+                {	
+                    byte data[] = serialPort.readBytes(event.getEventValue());
+                    for(int i = 0; i < data.length; i++)
+                    	handleReceivedByte(data[i]);
                 }
                 catch (SerialPortException ex) {
                     System.out.println(ex);
                 }
             }
         }
+    }
+    
+    public class BusyException extends Exception
+    {
+		private static final long serialVersionUID = 9109563675205651338L;
+    }
+    
+    public class NoPortsFoundException extends Exception {
+
+		private static final long serialVersionUID = -6206865497981467014L;
     }
 }
 
---
+++ Bytecode Uploader/src/se/neava/bytecodeuploader/UploadTestUi.java
@@ -0,0 +1,24 @@
+package se.neava.bytecodeuploader;
+
+import java.io.UnsupportedEncodingException;
+
+import jssc.SerialPortException;
+import se.neava.bytecodeuploader.BytecodeUploader.BusyException;
+import se.neava.bytecodeuploader.BytecodeUploader.NoPortsFoundException;
+
+public class UploadTestUi {
+
+	public static void main(String[] args) 
+	{
+		try 
+		{
+			BytecodeUploader bu = new BytecodeUploader(null, 9600);
+			bu.transmitCode("abcdefghijklmnopqrstuvwxyz".getBytes("UTF8"), 5);
+		} 
+		catch (NoPortsFoundException | SerialPortException | UnsupportedEncodingException | BusyException e) 
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
