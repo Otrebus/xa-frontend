96bca87f8a433e2c14f753103c9c1657780635a9
Otrebus <panzerschlacht@hotmail.com>
2014-07-14 18:08:46 +0200

    Messing about with git, trying to get a better structure

--- Bytecode Uploader/src/se/neava/bytecodeuploader/UploadTestUi.java
+++ Bytecode Uploader/src/se/neava/bytecodeuploader/UploadTestUi.java
@@ -1,24 +1,21 @@
-package se.neava.bytecodeuploader;
-
-import java.io.UnsupportedEncodingException;
-
-import jssc.SerialPortException;
-import se.neava.bytecodeuploader.BytecodeUploader.BusyException;
-import se.neava.bytecodeuploader.BytecodeUploader.NoPortsFoundException;
-
-public class UploadTestUi {
+import se.neava.bytecodeuploader.BytecodeUploader;
 
     public static void main(String[] args) 
     {
+        File file = new File("input.asm");
+        System.out.println(file.getAbsolutePath());
         try 
         { 
+            byte[] bytes = Files.readAllBytes(file.toPath());
+            String text = new String(bytes,"UTF-8");
+
+            new Assembler().assemble(text);
             BytecodeUploader bu = new BytecodeUploader(null, 9600);
             bu.transmitCode("abcdefghijklmnopqrstuvwxyz".getBytes("UTF8"), 5);
         } 
-        catch (NoPortsFoundException | SerialPortException | UnsupportedEncodingException | BusyException e) 
+        catch (NoPortsFoundException | SerialPortException | BusyException | IOException | ParseException e) 
         {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
     }
-}
